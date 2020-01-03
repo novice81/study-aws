d56b8f686bd870355628bc37c8d0182ba8dc862e
novice81 <novice81.github@gmail.com>
2020-01-03 23:24:43 +0200

    Add profile for the EC2 instance as stage

--- dynamodb/example-java/src/main/java/study/aws/example/dynamodb/ResourceProvider.java
+++ dynamodb/example-java/src/main/java/study/aws/example/dynamodb/ResourceProvider.java
@@ -1,16 +1,21 @@
 package study.aws.example.dynamodb;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 public class ResourceProvider {
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
 
     @Autowired
     private Environment environment;
@@ -19,8 +24,16 @@ public class ResourceProvider {
         return this.environment;
     }
 
+    public String getActiveProfile() {
+        return this.activeProfile;
+    }
+
     @Bean
     public AwsCredentialsProvider awsCredentialsProvider() {
+        if (activeProfile.equalsIgnoreCase("stage")) {
+            return InstanceProfileCredentialsProvider.create();
+        }
+
         return ProfileCredentialsProvider.create("dynamodb");
     }
 
