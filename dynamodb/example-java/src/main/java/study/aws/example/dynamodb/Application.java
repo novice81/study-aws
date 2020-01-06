d22a8a9bcc5fb1689937115a06519c9de77b4064
novice81 <novice81.github@gmail.com>
2020-01-06 23:51:17 +0200

    Remove unnecessary log

--- dynamodb/example-java/src/main/java/study/aws/example/dynamodb/Application.java
+++ dynamodb/example-java/src/main/java/study/aws/example/dynamodb/Application.java
@@ -11,7 +11,6 @@ public class Application {
     private static Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        logger.info("## TEST ##");
         SpringApplication.run(Application.class, args);
     }
 }
